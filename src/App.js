2b37440e1fa804873682da8154a10ac87a2dc2bf
alexandr_li <alexandr_li@epam.com>
2018-08-26 23:02:40 +0600

    Added first components. Added gh-pages configs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
   }
 }
 
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, {Component, Fragment} from 'react';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Burger />
+        <div>Build Controls</div>
+      </Fragment>
+    );
+  }
+}
+
+export default BurgerBuilder;
